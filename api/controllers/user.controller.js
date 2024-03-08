6b0e0266312eacbc2a54d4dc6ec7bfb505fe11a5
Radheshyam gupta <radheshyamjio52@gmail.com>
2024-03-08 07:18:04 +0530

    completed all getusers api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -19,27 +19,31 @@ export const updateUser = async (req, res, next) => {
 
   if (req.body.username) {
     if (req.body.username.length < 7 || req.body.username.length > 20) {
-      return next(ErrorHandler(401, "username length must be between 7 to 20 "));
+      return next(
+        ErrorHandler(401, "username length must be between 7 to 20 ")
+      );
     } else if (req.body.username.includes(" ")) {
       return next(ErrorHandler(401, "username can not contain spaces "));
     } else if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
-      return next(ErrorHandler(401, "username can only contain numbers and letters "));
+      return next(
+        ErrorHandler(401, "username can only contain numbers and letters ")
+      );
     }
   }
 
   try {
-    const update =   await User.findByIdAndUpdate(
-        req.params.userId,
-        {
-          $set: {
-            username: req.body.username,
-            email: req.body.email,
-            profilePicture: req.body.profilePicture,
-            password: req.body.password,
-          },
+    const update = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
         },
-        { new: true }
-      );
+      },
+      { new: true }
+    );
     const { password, ...rest } = update._doc;
     res.status(200).json(rest);
   } catch (error) {
@@ -47,27 +51,66 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
-
-
-
-export async function deleteUser(req,res,next){
+export async function deleteUser(req, res, next) {
   if (req.user.id !== req.params.userId) {
     return next(ErrorHandler(401, "not allowed"));
   }
 
   try {
     await User.findByIdAndDelete(req.params.userId);
-    res.status(200).json("user deleted successfully")
+    res.status(200).json("user deleted successfully");
   } catch (error) {
-    next(error)
+    next(error);
   }
 }
 
-
-export async function signOut(req,res,next){
+export async function signOut(req, res, next) {
   try {
-    res.clearCookie('blog_token').status(200).json("user has been signed out successfully")
+    res
+      .clearCookie("blog_token")
+      .status(200)
+      .json("user has been signed out successfully");
   } catch (error) {
-    next(error)
+    next(error);
+  }
+}
+
+export async function getUsers(req, res, next) {
+  if (!req.user.isAdmin) {
+    return next(ErrorHandler(401, "not allowed to see users"));
+  }
+
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDicrction = req.query.sort === "asc" ? 1 : -1;
+    const users = await User.find()
+      .sort({ createdAt: sortDicrction })
+      .skip(startIndex)
+      .limit(limit);
+    res.status(200).json(users);
+
+    const rmovePassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    const totalUsers = await User.countDocuments();
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastmonths = await User.find({ createdAt: { $gte: oneMonthAgo } });
+
+    res.status(200).json({
+      users: rmovePassword,
+      totalUsers,
+      lastmonths,
+    });
+  } catch (error) {
+    next(error);
   }
 }
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { updateUser, test, deleteUser, signOut } from '../controllers/user.controller.js';
+import { updateUser, test, deleteUser, signOut, getUsers } from '../controllers/user.controller.js';
 import { VerifyToken } from '../utils/VerifyToken.js';
 import { updatePost } from '../controllers/post.controller.js';
 const router = express.Router()
@@ -8,5 +8,5 @@ router.get('/test',test)
 router.put('/update/:userId',VerifyToken,updateUser)
 router.delete('/delete/:userId',VerifyToken,deleteUser)
 router.post('/updatepost',VerifyToken,updatePost)
-
+router.get('/getUsers',VerifyToken,getUsers)
 export default router;
